390a79ac07d380375e9be33c0fc5387d0a1e9cba
Fox <j.bosco1708@gmail.com>
2023-09-21 14:43:46 -0300

    Primeiro código de soma

--- primeiro projeto/src/App.java
+++ primeiro projeto/src/App.java
@@ -1,5 +1,14 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        int num1,num2,num3;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Digite um valor");
+        num1 = scanner.nextInt();
+        System.out.println("Digite outro número");
+        num2 = scanner.nextInt();
+        num3 = num1 + num2;
+        System.out.println("Resultado: "+num3);
     }
 }
