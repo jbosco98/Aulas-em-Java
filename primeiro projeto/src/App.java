76210eb2b66e7e71e5330661477884b3c3abd395
Fox <j.bosco1708@gmail.com>
2023-09-21 16:58:31 -0300

    Estrutura de repetição

--- primeiro projeto/src/App.java
+++ primeiro projeto/src/App.java
@@ -2,7 +2,12 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        
-
+        int n$;
+        Scanner cascata = new Scanner(System.in);
+        System.out.println("Digite o limite para o programa parar de repetir.");
+        n$ = cascata.nextInt();
+        for(int i=1; i<=n$;i++){
+        System.out.println(i+". Eu vou estudar programação.");
+        }
 }
 }
